9ea4cb2f022c967136a66fa36560b5a694a97e8d
yui666a <haiso666@gmail.com>
2021-06-12 21:32:04 +0900

    Complete!

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -2,25 +2,24 @@ import React, { useState, useEffect } from 'react'
 import { BreedsSelect } from './BreedsSelect'
 
 export const DogListContainer = () => {
-  const [breeds, setBreeds] = useState(0)
+  const [breeds, setBreeds] = useState([])
   const [selectedBreed, setSelectedBreed] = useState([])
 
   //生成時に実行
-  useEffect(() => {
+  useEffect(() => { // 確認！
     async function getBreedsList() {
       const result = await fetch('https://dog.ceo/api/breeds/list/all')
       var breeds_list = await result.json()
-      console.log(breeds_list)
       breeds_list = Object.keys(breeds_list.message)
       setBreeds(breeds_list)
     }
     getBreedsList()
-  }, [])
+  }, [setBreeds])
 
   //関数
   const showImgList = async showSelect => {
     const result = await fetch(
-      'https://dog.ceo/api/breed/' + showSelect + '/images/random/9',
+      'https://dog.ceo/api/breed/' + showSelect + '/images/random/12',
     )
     var img_list = await result.json()
     img_list = img_list.message
